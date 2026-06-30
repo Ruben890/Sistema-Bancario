28cbb6373c1b22cf9549f12d4f59c2cf63997e2e
Ruben890 <78616515+Ruben890@users.noreply.github.com>
2026-06-30 08:19:12 -0400

    Replace Vite basic SSL with mkcert

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import basicSsl from "@vitejs/plugin-basic-ssl";
+import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react(), mkcert()],
   server: {
     https: {},
     port: 5173,
